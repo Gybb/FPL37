056400319f28b4690fb104f68d176893b37ac02d
Gybb <fredrik_ljung@live.se>
2021-08-14 23:24:04 +0200

    Update AJS37.lua

--- AJS37.lua
+++ AJS37.lua
@@ -287,9 +287,9 @@ defineToggleSwitch("CB_EJECTION_SYSTEM", 18, 3909, 306, "Säkringspanel", "CB Ej
 defineToggleSwitch("CB_ENGINE", 18, 3910, 307, "Säkringspanel", "CB Engine")  --OK						ENGINEPANEL-18
 
 --[[--Autopilot--]]--
-definePushButton("SPAK", 22, 3301, 401,  "Autopilot",  "SPAK")  --OK 								FLIGHTDATAUNIT-22
-definePushButton("ATTITUDE_HOLD", 22, 3302, 402, "Autopilot", "Attitude Hold")  --OK 						FLIGHTDATAUNIT-22
-definePushButton("ALTITUDE_HOLD", 22, 3303, 403, "Autopilot", "Altitude Hold")  --OK 						FLIGHTDATAUNIT-22
+defineToggleSwitch("SPAK", 22, 3301, 401,  "Autopilot",  "SPAK")  --OK 								FLIGHTDATAUNIT-22
+defineToggleSwitch("ATTITUDE_HOLD", 22, 3302, 402, "Autopilot", "Attitude Hold")  --OK 						FLIGHTDATAUNIT-22
+defineToggleSwitch("ALTITUDE_HOLD", 22, 3303, 403, "Autopilot", "Altitude Hold")  --OK 						FLIGHTDATAUNIT-22
 defineIndicatorLight("SPAK_LAMP", 401, "Indikeringslampor", "SPAK Lamp")
 defineIndicatorLight("ATT_LAMP", 402, "Indikeringslampor", "ATT Lamp")
 defineIndicatorLight("HOJD_LAMP", 403, "Indikeringslampor", "HOJD Lamp")
